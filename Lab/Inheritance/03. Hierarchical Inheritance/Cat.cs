30b601383f7341aa2b99adec02431ceb8ef9dac2
zaprian1223 <zaprian1223@gmail.com>
2022-08-04 16:25:03 +0300

    Added a solution

---
+++ Lab/Inheritance/03. Hierarchical Inheritance/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+   public class Cat : Animal
+    {
+        public virtual void Meow()
+        {
+            Console.WriteLine("meowing");
+        }
+    }
+}
---
+++ Lab/Inheritance/03. Hierarchical Inheritance/Puppy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+   public class Puppy : Dog
+    {
+        public virtual void Weep()
+        {
+            Console.WriteLine("weeping");
+        }
+             
+    }
+}
