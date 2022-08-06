bbd96cef7600eb7e126cd58a64f6fbc7fe21ba8c
zaprian1223 <zaprian1223@gmail.com>
2022-08-06 16:47:38 +0300

    Added a solution

---
+++ Lab/Inheritance/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string>() { "Ivan", "Pesho" });
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
