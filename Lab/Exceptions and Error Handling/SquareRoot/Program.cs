8988a3153b4726dbdffb994748c8ec6ee3a56b05
zaprian1223 <zaprian1223@gmail.com>
2022-08-14 12:57:59 +0300

    Added a solution

---
+++ Lab/Exceptions and Error Handling/SquareRoot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            try
+            {
+
+                int number = Convert.ToInt32(Console.ReadLine());
+                double root = 1;
+                int i = 0;
+                if (number < 0)
+                {
+                    throw new Exception("Invalid number.");
+                }
+                while (true)
+                {
+                    i = i + 1;
+                    root = (number / root + root) / 2;
+                    if (i == number + 1) { break; }
+                }
+                Console.WriteLine((int)root);
+                Console.WriteLine("Goodbye.");
+            }
+            catch (Exception ax)
+            {
+
+                Console.WriteLine(ax.Message);
+                Console.WriteLine("Goodbye.");
+            }
+            
+
+           
+        }
+    }
+}
